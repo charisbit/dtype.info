2916396c3b48bae86e041c238cd4e04b1cd87353
saintway @ Andrews-MacBook-Pro <saintway.sakaru@gmail.com>
2022-02-28 23:53:25 +0900

    Site updated: 2022-02-28 23:53:25

---
+++ js/twitter.js
@@ -0,0 +1,33 @@
+var twitter = (function() {
+  function escapeHtml(str) {
+    return $('<div/>').text(str).html();
+  }
+  function render(target, tweets) {
+    var i = 0, fragment = '', t = $(target)[0];
+
+    for(i = 0; i < tweets.length; ++i) {
+      fragment += '<li><i class="fa fa-star"></i><a href="https://twitter.com/digitypography/status/' + tweets[i].id + '">'
+          + tweets[i].created_at.replace('T', '　').replace('.000Z', '　UTC') + '</a><p>' + escapeHtml(tweets[i].text||'') + '</p></li>';
+    }
+    t.innerHTML = fragment;
+  }
+  return {
+    showTweets: function(options) {
+      options.blacklist = options.blacklist.split(',');
+      $.ajax({
+          url: 'https://jhub.name/typography/?tweet.fields=created_at&max_results=' + options.count
+        , dataType: 'json'
+        , error: function (err) { $(options.target + ' li.loading').addClass('error').text('Error loading feed'); }
+        , success: function (data) {
+          if (!data ) { return; }
+          var tweets = [];
+          for (var i = 0; i < data.data.length; ++i) {
+            if (options.blacklist instanceof Array && options.blacklist.indexOf(data.data[i].id) !== -1) { continue; }
+            tweets.push(data.data[i]);
+          }
+          render(options.target, tweets);
+        }
+      });
+    }
+  };
+})();
