1ed12bfc1638ff654f1952a1908178d2c15c5eb8
Hijirii <saintway@dtype.info>
2026-02-15 22:55:51 +0900

    Site updated: 2026-02-15 22:55:50

--- js/twitter.js
+++ js/twitter.js
@@ -1,33 +1,13 @@
 var twitter = (function() {
-  function escapeHtml(str) {
-    return $('<div/>').text(str).html();
-  }
-  function render(target, tweets) {
-    var i = 0, fragment = '', t = $(target)[0];
-
-    for(i = 0; i < tweets.length; ++i) {
-      fragment += '<li><i class="fa fa-star"></i><a href="https://twitter.com/charisbit/status/' + tweets[i].id + '">'
-          + tweets[i].created_at.replace('T', '　').replace('.000Z', '　UTC') + '</a><p>' + escapeHtml(tweets[i].text||'') + '</p></li>';
-    }
-    t.innerHTML = fragment;
+  function render(target) {
+    var fragment = '<li><i class="fa fa-twitter"></i><a href="https://twitter.com/charisbit" target="_blank">@charisbit</a><p>Follow on Twitter</p></li>';
+    $(target)[0].innerHTML = fragment;
   }
   return {
     showTweets: function(options) {
-      options.blacklist = options.blacklist.split(',');
-      $.ajax({
-          url: 'https://jhub.dtype.info/typography/?tweet.fields=created_at&max_results=' + options.count
-        , dataType: 'json'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text('Error loading feed'); }
-        , success: function (data) {
-          if (!data ) { return; }
-          var tweets = [];
-          for (var i = 0; i < data.data.length; ++i) {
-            if (options.blacklist instanceof Array && options.blacklist.indexOf(data.data[i].id) !== -1) { continue; }
-            tweets.push(data.data[i]);
-          }
-          render(options.target, tweets);
-        }
-      });
+      // Use Twitter native embed
+      var twitterHtml = '<a class="twitter-timeline" href="https://twitter.com/charisbit?ref_src=twsrc%5Etfw">Tweets by @charisbit</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"><\/script>';
+      $(options.target).html(twitterHtml);
     }
   };
 })();
