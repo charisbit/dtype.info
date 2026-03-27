5434c2049455dfd1b8c6def233972dc341532514
Hijirii <saintway@dtype.info>
2026-03-27 18:47:46 +0900

    Restore github.js and twitter.js, update blog content

---
+++ js/twitter.js
@@ -0,0 +1,13 @@
+var twitter = (function() {
+  function render(target) {
+    var fragment = '<li><i class="fa fa-twitter"></i><a href="https://twitter.com/charis_angelos" target="_blank">@charis_angelos</a><p>Follow on Twitter</p></li>';
+    $(target)[0].innerHTML = fragment;
+  }
+  return {
+    showTweets: function(options) {
+      // Use Twitter native embed
+      var twitterHtml = '<a class="twitter-timeline" href="https://twitter.com/charis_angelos?ref_src=twsrc%5Etfw">Tweets by @charis_angelos</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"><\/script>';
+      $(options.target).html(twitterHtml);
+    }
+  };
+})();
